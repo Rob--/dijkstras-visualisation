6e533dc8271d0a7caf6e1d57b6a8d33cce4c0fef
Robert Valentyne <valentyne.robert@gmail.com>
2018-03-14 01:23:55 +0000

    added settings

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-const tree = new Tree(window.innerWidth, window.innerHeight);
-tree.generateTree();
+let NODE_COUNT = 15;
+let EDGE_COUNT = 5;
+
+let tree;
+let paused = false;
+
+(window.regen = function() {
+  tree = new Tree(window.innerWidth, window.innerHeight, NODE_COUNT, EDGE_COUNT);
+  tree.generateTree();
+}());
 
 const cy = cytoscape({
   container: document.getElementById('canvas'),
@@ -17,7 +25,7 @@ const cy = cytoscape({
   }
 });
 
-setInterval(() => {
+function main() {
   // clear the tree
   cy.$('').remove();
 
@@ -29,10 +37,44 @@ setInterval(() => {
   cy.add(tree.format());
 
   for (let i = 0; i < path.length - 1; i++) {
-    // select edge connecting two nodes
+    // select edge connecting two nodes,
+    // edge id is made up as: [node 1 id][node 2 id],
+    // select both `[node 1 id][node 2 id]` and `[node 2 id][node 1 id]`
+    // due to the way edges are added
     cy.$(`#${path[i]}${path[i + 1]}`).select();
+    cy.$(`#${path[i + 1]}${path[i]}`).select();
 
     // select the node
     cy.$(`#${path[i + 1]}`).select();
   }
-}, 40);
+}
+
+let interval = setInterval(main, 50);
+
+$('#interval').change(function() {
+  $('#i').text($(this).val());
+  clearInterval(interval);
+  interval = setInterval(main, $(this).val());
+});
+
+$('#nodes').change(function() {
+  $('#n').text($(this).val());
+  NODE_COUNT = $(this).val();
+  regen()
+});
+
+$('#edges').change(function() {
+  $('#e').text($(this).val());
+  EDGE_COUNT = $(this).val();
+  regen()
+});
+
+$('#pause').click(function() {
+  paused = !paused;
+
+  if (paused) {
+    clearInterval(interval);
+  } else  {
+    interval = setInterval(main, $('#interval').val());
+  }
+});
